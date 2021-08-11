492ef97136c55c9209a27e8666f54276ba9b1b50
Apoorva <apoorvamandhare2001@gmail.com>
2021-08-11 19:43:18 +0530

    initial commit

---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnmr5GboLUI32W-hyMQbP9P2CkBMKyD_U",
+    authDomain: "quiz-react-89669.firebaseapp.com",
+    projectId: "quiz-react-89669",
+    storageBucket: "quiz-react-89669.appspot.com",
+    messagingSenderId: "692143678254",
+    appId: "1:692143678254:web:cd8a2cd6d10060d3c6d0bf"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
